19b6e13ee6e8fa661572deca0a21fdda44e412db
myeongyolee <myeongyolee@gmail.com>
2019-07-11 12:07:19 +0900

    sora commit complete

--- Awesome/bin/src/main/webapp/WEB-INF/views/common/msg.jsp
+++ Awesome/bin/src/main/webapp/WEB-INF/views/common/msg.jsp
@@ -2,5 +2,9 @@
     pageEncoding="UTF-8"%>
 <script>
 alert("${msg}");
+<<<<<<< HEAD
 location.href ="${pageContext.request.contextPath}${loc}";
+=======
+location.href="${pageContext.request.contextPath}${loc}";
+>>>>>>> refs/remotes/origin/jinwoo
 </script>
--- Awesome/src/main/java/com/kh/awesome/common/typeHandler/StringToLongTypeHandler.java
+++ Awesome/src/main/java/com/kh/awesome/common/typeHandler/StringToLongTypeHandler.java
@@ -7,39 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.apache.ibatis.type.JdbcType;
-<<<<<<< HEAD
-import org.apache.ibatis.type.TypeHandler;
 
-public class StringToLongTypeHandler implements TypeHandler<String>{
-
-	@Override
-	public void setParameter(PreparedStatement ps, int i, String parameter, JdbcType jdbcType) throws SQLException {
-		String s = (String) parameter;
-
-        StringReader reader = new StringReader(s);
-
-        ps.setCharacterStream(i, reader, s.length());
-	}
-
-	@Override
-	public String getResult(ResultSet rs, String columnName) throws SQLException {
-		return rs.getString(columnName);
-	}
-
-	@Override
-	public String getResult(ResultSet rs, int columnIndex) throws SQLException {
-		return rs.getString(columnIndex);
-	}
-
-	@Override
-	public String getResult(CallableStatement cs, int columnIndex) throws SQLException {
-		return cs.getString(columnIndex);
-	}
-
-	
-
-}
-=======
 import org.apache.ibatis.type.MappedJdbcTypes;
 import org.apache.ibatis.type.TypeHandler;
 
@@ -73,4 +41,4 @@ public class StringToLongTypeHandler implements TypeHandler<String>{
 	
 
 }
->>>>>>> refs/remotes/origin/sora
+
